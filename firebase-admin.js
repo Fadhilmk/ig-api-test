03693383aa6f7f58b07f6c2889c053ba70f24cae
Fadhil <boggaming098.com@gmail.com>
2024-10-22 02:41:24 +0530

    webhook post req set updation14

--- firebase-admin.js
+++ firebase-admin.js
@@ -10,19 +10,15 @@
 // module.exports = admin;
 
 import admin from 'firebase-admin';
-import { initializeApp, applicationDefault, cert } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 // Initialize Firebase Admin SDK only if it's not already initialized
-if (!admin.apps.length) {
-  initializeApp({
-    credential: cert(serviceAccount),
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
     databaseURL: process.env.FIREBASE_DATABASE_URL,
-  });
-}
+});
 
-const db = getFirestore();
 
-export { admin, db };
+export { admin };
